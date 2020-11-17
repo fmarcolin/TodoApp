1a53040d246c867d6e650a561c5c7cd51070ce4f
Felipe Marcolin <felipe.marcolin@hotmail.com>
2020-11-17 11:16:31 -0300

    DbContext and Queries
    Created DbContext, queries, and new test

---
+++ Todo.Domain.Tests/QueriesTests/TodoQueriesTests.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Todo.Domain.Entities;
+using Todo.Domain.Queries;
+
+namespace Todo.Domain.Tests.QueriesTests
+{
+    [TestClass]
+    public class TodoQueriesTests
+    {
+        private List<TodoItem> _items;
+
+        public TodoQueriesTests()
+        {
+            _items = new List<TodoItem>();
+            _items.Add(new TodoItem("title 1", "user", DateTime.Now));
+            _items.Add(new TodoItem("title 2", "another user", DateTime.Now));
+            _items.Add(new TodoItem("title 3", "another user", DateTime.Now));
+            _items.Add(new TodoItem("title 4", "user", DateTime.Now));
+            _items.Add(new TodoItem("title 5", "user", DateTime.Now));
+            _items.Add(new TodoItem("title 6", "another user", DateTime.Now));
+            _items.Add(new TodoItem("title 7", "another user", DateTime.Now));
+            _items.Add(new TodoItem("title 8", "user", DateTime.Now));
+        }
+
+        [TestMethod]
+        public void ShoudReturnJustUserTasks()
+        {
+            var result = _items.AsQueryable().Where(TodoQueries.GetAll("user"));
+            Assert.AreEqual(result.Count(), 4);
+        }
+    }
+}
---
+++ Todo.Domain/Queries/TodoQueries.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq.Expressions;
+using Todo.Domain.Entities;
+
+namespace Todo.Domain.Queries
+{
+    public static class TodoQueries
+    {
+        public static Expression<Func<TodoItem, bool>> GetAll(string User)
+        {
+            return x => x.User == User;
+        }
+
+        public static Expression<Func<TodoItem, bool>> GetAllDone(string User)
+        {
+            return x => x.User == User && x.Done;
+        }
+
+        public static Expression<Func<TodoItem, bool>> GetAllUndone(string User)
+        {
+            return x => x.User == User && !x.Done;
+        }
+
+        public static Expression<Func<TodoItem, bool>> GetByDay(string User, DateTime date, bool done)
+        {
+            return x => 
+                x.User == User && 
+                x.Done == done &&
+                x.Date.Date == date.Date;
+        }
+    }
+}
